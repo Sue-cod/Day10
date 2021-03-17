d025eb2e7da840e1b19773b54960256dbc813d89
Sue-cod <snozic892@gmail.com>
2021-03-17 18:48:22 +0100

    Prvi zadatak

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+var myPet = {
+    species: "Maltezer",
+    name: "Maia",
+    legs: 4,
+    friends: ["Luigi", "Garfield"]
+
+}
+
+function myFunction(myPet) {
+return myPet;
+}
+// Only change code above this line
+
+
+console.log(myFunction(myPet));  // Change this line
+module.exports = { myPet, myFunction };
